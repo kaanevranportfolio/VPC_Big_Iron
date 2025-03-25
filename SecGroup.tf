8cc18de4d5dded49d39f05d0f24d22f4404de3fd
kemechial <kaanevran@gmail.com>
2025-03-25 10:49:14 +0300

    nginx.conf edited

--- SecGroup.tf
+++ SecGroup.tf
@@ -13,6 +13,7 @@ resource "aws_security_group" "nginx_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -92,6 +93,7 @@ resource "aws_security_group" "web_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
